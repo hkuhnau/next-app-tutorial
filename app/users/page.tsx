634853811fef687bb05caa8dbdba79f059cf3474
Heidi Kuhnau <hkuhnau@trident-it.com>
2024-08-01 13:10:14 -0500

    changing styles

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
+interface User {
+  id: number;
+  name: string;
+}
+
 const UsersPage = async () => {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const users = await res.json();
+  const users: User[] = await res.json();
   return (
-    <div>UsersPage</div>
+    <>
+      <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul>
+        {users.map(user => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
   )
 }
 
